073bc72f3459cda3e7d3841f83c428a14efc86de
Tristan | S. <simontristandev@gmail.com>
2025-05-27 13:10:30 +0200

    remove the 6h and make all to 12h

--- Update[Payloader]/update-timer.js
+++ Update[Payloader]/update-timer.js
@@ -33,15 +33,15 @@ let total_update_count_csv = parseInt(args_update_count[0])
 isNaN (total_update_count_csv) ? total_update_count_csv = 0 : total_update_count_csv
 total_update_count_csv += 1 //add +1 to the total update_count for the csv file
 
-//this is for the latest update[6 hours] .txt file
+//this is for the latest update[12 hours] .txt file
 let update_count_txt = parseInt(args_txt[0]) //uses the first number/string on the beginning of the txt document args[0] :number
 isNaN (update_count_txt) ? update_count_txt = 0 : update_count_txt; //check if its a number
 update_count_txt += 1 //add +1 to the update count
 
 
-//example: date_string = 4 Total Updates in the last 6 hours between 14 - 20
-let date_string = `${update_count_txt} Total Updates in the last 6 hours between ${args_txt[9]} - ${args_txt[11]}`;
-//date_string = `${update_count_txt} Total Updates in the last 6 hours between ${date_args.month}/${date_args.day}/${date_args.year}...${date_args.hour}:${date_args.minutes} - ${date_args.month}/${date_args.day}/${date_args.year}...${date_args.hour + 6}:${date_args.minutes}UTC`;
+//example: date_string = 4 Total Updates in the last 12 hours between 14 - 20
+let date_string = `${update_count_txt} Total Updates in the last 12 hours between ${args_txt[9]} - ${args_txt[11]}`;
+//date_string = `${update_count_txt} Total Updates in the last 12 hours between ${date_args.month}/${date_args.day}/${date_args.year}...${date_args.hour}:${date_args.minutes} - ${date_args.month}/${date_args.day}/${date_args.year}...${date_args.hour + 6}:${date_args.minutes}UTC`;
 
 //the update string for the csv file
 const csv_date_string = `\nUpdate on ${date_args.month}/${date_args.day}/${date_args.year}...${date_args.hour}:${date_args.minutes}UTC,${total_update_count_csv}`
